ce413fff624560aed11ef4d7f5b4e9fb667ebc03
sofia-tamayosepulveda11 <123569358+sofia-tamayosepulveda11@users.noreply.github.com>
2023-02-06 12:06:22 -0500

    vars

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "inglaterra",
+      img: "img/inglaterra box.jpg"
+    },
+    {
+      name: "canada",
+      img: "img/canada box.jpg"
+    },
+    {
+      name: "dubai",
+      img: "img/dubai box.jpg"
+    },
+    {
+      name: "madrid",
+      img: "img/madrid box.jpg"
+    },
+    {
+      name: "new york",
+      img: "img/new york box.jpg"
+    },
+    {
+      name: "paris",
+      img: "img/paris box.jpg"
+    },
+    {
+      name: "inglaterra",
+      img: "img/inglaterra box.jpg"
+    },
+    {
+      name: "canada",
+      img: "img/canada box.jpg"
+    },
+    {
+      name: "dubai",
+      img: "img/dubai box.jpg"
+    },
+    {
+      name: "madrid",
+      img: "img/madrid box.jpg"
+    },
+    {
+      name: "new york",
+      img: "img/new york box.jpg"
+    },
+    {
+      name: "paris",
+      img: "img/paris box.jpg"
+    }
+  ];
+});
